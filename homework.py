834c126d943c78a2db804025122ceec37729cae7
Ivan Pavlov <divaythfyr@mail.ru>
2023-01-31 23:41:12 +0300

    Add comments

--- homework.py
+++ homework.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 @dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
+
     training_type: str
     duration: float
     distance: float
@@ -20,7 +21,11 @@ class InfoMessage:
 
 
 class Training:
-    """Базовый класс тренировки."""
+    """Базовый класс тренировки.
+    Содержит константы для перевода километров в метры и часов в минуты,
+    а также длину шага в метрах.
+    Принимает количество шагов, время тренировки в часах, вес в килограммах."""
+
     M_IN_KM: int = 1000
     MIN_IN_HOUR: int = 60
     LEN_STEP: float = 0.65
@@ -35,19 +40,20 @@ class Training:
         self.weight: float = weight
 
     def get_distance(self) -> float:
-        """Получить дистанцию в км."""
+        """Получить дистанцию тренировки в километрах."""
         return self.action * self.LEN_STEP / self.M_IN_KM
 
     def get_mean_speed(self) -> float:
-        """Получить среднюю скорость движения."""
+        """Получить среднюю скорость движения в километрах в час."""
         return self.get_distance() / self.duration
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        raise NotImplementedError('Опишите get_spent_calories в подклассах!')
+        raise NotImplementedError('В дочерних классах не описан метод '
+                                  'get_spent_calories.')
 
     def show_training_info(self) -> InfoMessage:
-        """Вернуть информационное сообщение о выполненной тренировке."""
+        """Вернуть объект, содержащий информацию о выполненной тренировке."""
         return InfoMessage(self.__class__.__name__,
                            self.duration,
                            self.get_distance(),
@@ -56,7 +62,9 @@ class Training:
 
 
 class Running(Training):
-    """Тренировка: бег."""
+    """Тренировка: бег.
+    Добавлены специальные константы для вычисления затраченных калорий."""
+
     CALORIES_MEAN_SPEED_MULTIPLIER: int = 18
     CALORIES_MEAN_SPEED_SHIFT: float = 1.79
 
@@ -69,7 +77,12 @@ class Running(Training):
 
 
 class SportsWalking(Training):
-    """Тренировка: спортивная ходьба."""
+    """Тренировка: спортивная ходьба.
+    Добавлены константы для перевода километров в час в метры в секундах,
+    метров в сантиметры.
+    Добавлены специальные константы для вычисления затраченных калорий.
+    Дополнительно принимает рост в сантиметрах."""
+
     MSEC_IN_KMHOUR: float = 0.278
     SM_IN_M: int = 100
     CALORIES_WEIGHT_FIRST_MULTIPLIER: float = 0.035
@@ -94,7 +107,11 @@ class SportsWalking(Training):
 
 
 class Swimming(Training):
-    """Тренировка: плавание."""
+    """Тренировка: плавание.
+    Переопределена константа длины шага: изменена на длину гребка в метрах.
+    Добавлены специальные константы для вычисления затраченных калорий.
+    Дополнительно принимает длину бассейна в метрах и количество заплывов."""
+
     LEN_STEP: float = 1.38
     CALORIES_SPEED_SHIFT: float = 1.1
     CALORIES_SPEED_MULTIPLIER: int = 2
@@ -111,10 +128,12 @@ class Swimming(Training):
         self.count_pool = count_pool
 
     def get_mean_speed(self) -> float:
+        """Получить среднюю скорость движения в километрах в час."""
         return (self.length_pool * self.count_pool / self.M_IN_KM
                 / self.duration)
 
     def get_spent_calories(self) -> float:
+        """Получить количество затраченных калорий."""
         return ((self.get_mean_speed() + self.CALORIES_SPEED_SHIFT)
                 * self.CALORIES_SPEED_MULTIPLIER * self.weight * self.duration)
 
@@ -122,6 +141,8 @@ class Swimming(Training):
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
     training_types = {'RUN': Running, 'WLK': SportsWalking, 'SWM': Swimming}
+    if workout_type not in training_types:
+        raise ValueError('Неправильный тип тренировки.')
     return training_types[workout_type](*data)
 
 
