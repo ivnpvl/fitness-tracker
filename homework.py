cad9f26c734fdad67df339d13b09cd538c16cea4
Ivan Pavlov <divaythfyr@mail.ru>
2023-02-03 15:15:32 +0300

    Add line brakes

--- homework.py
+++ homework.py
@@ -157,7 +157,8 @@ class Swimming(Training):
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения в километрах в час."""
         return (
-            self.length_pool * self.count_pool / self.M_IN_KM / self.duration
+            self.length_pool * self.count_pool / self.M_IN_KM
+            / self.duration
         )
 
     def get_spent_calories(self) -> float:
